4dbce8eaf4d8423d32f2ca26ed16aeefc29a49e7
Jadounix <50082586+Jadounix@users.noreply.github.com>
2020-03-02 11:53:07 +0100

    Delete connect.php

--- connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-try {
-  $bdd = new PDO("mysql:host=localhost;dbname=site_quiz;charset=utf8",
-  "test","test",array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));;}
-catch (Exception $e)
-{
-  die('Erreur fatale:'.$e->getMessage());
-}
-?>
