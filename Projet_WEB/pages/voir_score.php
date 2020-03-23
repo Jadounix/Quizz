f6f5654e4961c98b25601c3f68e24bf2ede489d9
JulietteG001 <61521095+JulietteG001@users.noreply.github.com>
2020-03-23 13:20:11 +0100

    J'ai presque rien fait mais je dois le commit pour pouvoir pull

--- Projet_WEB/pages/voir_score.php
+++ Projet_WEB/pages/voir_score.php
@@ -38,12 +38,13 @@
    while($Tuple_score=$data_score->fetch())
    {
      if($Tuple_score['login_joueur']==$_SESSION['login_entre'])
-     {
-        while($Tuple_quiz=$data_quiz->fetch()){ //On cherche le nom du quiz associé au numéro du quiz
-          if($Tuple_quiz['no_quiz']==$Tuple_score['no_quiz']){
-            echo '<h3>'.$Tuple_quiz['nom'].'</h3>';
-          }
-        } ?>
+   {
+      while($Tuple_quiz=$data_quiz->fetch()){ //On cherche le nom du quiz associé au numéro du quiz
+        if($Tuple_quiz['no_quiz']==$Tuple_score['no_quiz']){
+          echo '<h3>'.$Tuple_quiz['nom'].'</h3>';
+        }
+      } ?>
+
       <div class="bloc_quiz" id="quiz<?php echo $cpt ?>">
        <table class="table">
         <thead>
