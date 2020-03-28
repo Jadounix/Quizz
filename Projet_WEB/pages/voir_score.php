5891ba6b14ba502920985eef039f917395975d44
JulietteG001 <61521095+JulietteG001@users.noreply.github.com>
2020-03-28 12:56:30 +0100

    Affichage des meilleurs scores par quiz fini

--- Projet_WEB/pages/voir_score.php
+++ Projet_WEB/pages/voir_score.php
@@ -21,54 +21,52 @@
    <h2> Mes scores </h2>
 
    <?php
-   //Interrogation de la base de données des scores
-   require("../bdd/connect.php");
-   $req_score = 'SELECT * FROM SCORE';
-   $data_score = $bdd->query($req_score);
-
    //Interrogation de la base de données des quiz pour retrouver le nom du quiz
    require("../bdd/connect.php");
    $req_quiz = 'SELECT * FROM QUIZ';
    $data_quiz = $bdd->query($req_quiz);
 
-   $cpt=1;
+   //Interrogation de la base de données des scores
+   require("../bdd/connect.php");
+   $req_score = 'SELECT MAX(nb_points) as nb_points, no_quiz, temps FROM SCORE GROUP BY no_quiz';
+   $data_score = $bdd->query($req_score);
+   $tab_score=[];
 
-   while($Tuple_score=$data_score->fetch())
-   {
-     if($Tuple_score['login_joueur']==$_SESSION['login_entre'])
-     {
-       ?>
+   //les scores sont stockés par quiz dans un tableau de tableaux
+   while($Tuple_score=$data_score->fetch()){
+    $tab_score[]=['no_quiz'=>$Tuple_score['no_quiz'], 'Temps réalisé'=>$Tuple_score['temps'], 'Score réalisé'=>$Tuple_score['nb_points']];
+  }
+   //print_r($tab_score); //pour afficher le tableau pour les tests
 
-      <div class="" id="quiz<?php echo $cpt ?>">
-       <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">Nom du quiz</th>
-            <th scope="col">Temps réalisé</th>
-            <th scope="col">Score réalisé</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td><?php while($Tuple_quiz=$data_quiz->fetch()) //On cherche le nom du quiz associé au numéro du quiz
-            {
-              if($Tuple_quiz['no_quiz']==$Tuple_score['no_quiz'])
-              {
-                echo $Tuple_quiz['nom']; //PB tous les noms de s'affichent pas :'(
-              }
-            } ?></td>
-            <td><?php echo (int) ($Tuple_score['temps']/60)."min ".($Tuple_score['temps']%60)."s"?></td>
-            <td><?php echo $Tuple_score['nb_points'] ?></td>
-          </tr>
-        </tbody>
-       </table>
-     </div>
-     <br/>
-       <?php
-       $cpt++;
-     }
+   //Affichage des scores
+   $i=1;
+   foreach($tab_score as $sous_tab){ ?>
+     <div class="bloc" id="quiz<?php echo $i ?>">
+      <table class="table">
+       <thead>
+         <tr>
+           <th scope="col">Nom du quiz</th>
+           <th scope="col">Temps réalisé</th>
+           <th scope="col">Score réalisé</th>
+         </tr>
+       </thead>
+       <tbody>
+         <tr>
+           <td><?php while($Tuple_quiz=$data_quiz->fetch()) //On cherche le nom du quiz associé au numéro du quiz
+           {
+             if($Tuple_quiz['no_quiz']==$sous_tab['no_quiz'])
+             {
+               echo $Tuple_quiz['nom'];
+             }
+           } ?></td>
+           <td><?php echo (int) ($sous_tab['Temps réalisé']/60)."min ".($sous_tab['Temps réalisé']%60)."s"?></td>
+           <td><?php echo $sous_tab['Score réalisé'] ?></td>
+         </tr>
+       </tbody>
+     </table></div><br/>
+      <?php $i++;
    }
-     ?>
+   ?>
 
    <?php include '../includes/footer.php'; ?>
    <?php include '../lib/bootstrap_footer.php'; ?>
---
+++ Projet_WEB/pages/voir_score_V0.php
@@ -0,0 +1,78 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Voir mes quiz</title>
+  <meta name="description">
+
+  <?php include '../lib/bootstrap_header.php'; ?>
+
+</head>
+
+<body>
+
+   <?php include '../includes/menu_deconnexion.php'; ?>
+
+   <h2> Mes scores </h2>
+
+   <?php
+   //Interrogation de la base de données des scores
+   require("../bdd/connect.php");
+   $req_score = 'SELECT * FROM SCORE';
+   $data_score = $bdd->query($req_score);
+
+   //Interrogation de la base de données des quiz pour retrouver le nom du quiz
+   require("../bdd/connect.php");
+   $req_quiz = 'SELECT * FROM QUIZ';
+   $data_quiz = $bdd->query($req_quiz);
+
+   $cpt=1;
+
+   while($Tuple_score=$data_score->fetch())
+   {
+     if($Tuple_score['login_joueur']==$_SESSION['login_entre'])
+     {
+       ?>
+
+      <div class="" id="quiz<?php echo $cpt ?>">
+       <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Nom du quiz</th>
+            <th scope="col">Temps réalisé</th>
+            <th scope="col">Score réalisé</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td><?php while($Tuple_quiz=$data_quiz->fetch()) //On cherche le nom du quiz associé au numéro du quiz
+            {
+              if($Tuple_quiz['no_quiz']==$Tuple_score['no_quiz'])
+              {
+                echo $Tuple_quiz['nom']; //PB tous les noms de s'affichent pas :'(
+              }
+            } ?></td>
+            <td><?php echo (int) ($Tuple_score['temps']/60)."min ".($Tuple_score['temps']%60)."s"?></td>
+            <td><?php echo $Tuple_score['nb_points'] ?></td>
+          </tr>
+        </tbody>
+       </table>
+     </div>
+     <br/>
+       <?php
+       $cpt++;
+     }
+   }
+     ?>
+
+   <?php include '../includes/footer.php'; ?>
+   <?php include '../lib/bootstrap_footer.php'; ?>
+
+ </body>
+
+ </html>
