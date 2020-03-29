327afd7a8d2a4e905b082672aba7ae8938213de9
JulietteG001 <61521095+JulietteG001@users.noreply.github.com>
2020-03-29 15:23:58 +0200

    Onglet des pages

--- Projet_WEB/pages/creation_question.php
+++ Projet_WEB/pages/creation_question.php
@@ -3,7 +3,7 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Créer un quiz</title>
     <?php include '../lib/bootstrap_header.php'; ?>
   </head>
   <body>
