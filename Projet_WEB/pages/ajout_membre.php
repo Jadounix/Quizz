a6d8e1ab720a43757d95ae2eda4c271e5a835fc6
Jadounix <jademariepetit@yahoo.fr>
2020-03-31 12:18:11 +0200

    Modification des commentaires et debogage

--- Projet_WEB/pages/ajout_membre.php
+++ Projet_WEB/pages/ajout_membre.php
@@ -13,44 +13,51 @@
 
   require("../bdd/connect.php");
 
+  //Infos rentrées par l'utilisateur
   $nouvLog = $_POST['login_entre'];
   $nouvMdp = $_POST['pass_entre'];
 
   $log = 0; //Variable qui vaut 0 si le login est libre, 1 sinon
 
   //On veut vérifier que le login n'est pas déjà dans une base de données
+  //On parcourt la bdd des joueurs
   $reqJoueur = 'SELECT login_joueur FROM JOUEUR';
   $dataJoueur = $bdd->query($reqJoueur);
   while($TupleJoueur=$dataJoueur->fetch())
   {
     if($TupleJoueur['login_joueur']==$nouvLog)
     {
+      //Le login est déjà utilisé : la variable vaut 1
       $log = 1;
     }
   }
-
+  //On parcourt la bdd des admins
   $reqAd = 'SELECT login_ad FROM ADMIN';
   $dataAd = $bdd->query($reqAd);
   while($TupleAd=$dataAd->fetch())
   {
     if($TupleAd['login_ad']==$nouvLog)
     {
+      //Le login est déjà utilisé : la variable vaut 1
       $log = 1;
     }
   }
 
-  if(isset($_POST['statutA']) and $log==0)
+  //Le login n'a pas été utilisé donc log vaut 0
+  //$_POST['statutJ'] existe, donc la personne a choisi d'être joueur
+  if(isset($_POST['statutJ']) and $log == 0)
   {
-    //La personne est un JOUEUR -> A jout à la base de données
+    //Ajout à la base de données
     $requete = $bdd->prepare("INSERT INTO JOUEUR(login_joueur,mdp_joueur) VALUES (:login,:mdp)");
     $requete->bindValue('login',$nouvLog,PDO::PARAM_STR);
     $requete->bindValue('mdp',$nouvMdp,PDO::PARAM_INT);
     $requete->execute();
     echo 'Vous avez bien été inscrit ! Vous pouvez désormais vous connecter.';
   }
-  elseif (isset($_POST['statutJ']) and $log==0)
+  //$_POST['statutA'] existe, donc la personne a choisi d'être joueur
+  elseif (isset($_POST['statutA']) and $log == 0)
   {
-    //La personne est ADMIN -> A jout à la base de données
+    //Ajout à la base de données
     $requete = $bdd->prepare("INSERT INTO ADMIN(login_ad,mdp_ad) VALUES (:login,:mdp)");
     $requete->bindValue('login',$nouvLog,PDO::PARAM_STR);
     $requete->bindValue('mdp',$nouvMdp,PDO::PARAM_INT);
@@ -61,9 +68,9 @@
   {
     echo "Vous n'avez pas choisi de statut, ou le login choisi a déjà été utilisé.";
   }
-  echo "<br/>";
-  echo '<a class="lien" href="inscription.php">➔ Revenir à la page précédente.</a>';
-   ?>
+  ?>
+  <br/>
+  <a class="lien" href="inscription.php">➔ Revenir à la page précédente.</a>
 
   <?php include '../includes/footer.php'; ?>
   <?php include '../lib/bootstrap_footer.php'; ?>
--- Projet_WEB/pages/inscription.php
+++ Projet_WEB/pages/inscription.php
@@ -10,16 +10,21 @@
     <?php include '../includes/menu_connexion.php'; ?>
 
     <h1>S'inscrire sur QuizCeption !</h1>
+
+    <!-- Formulaire envoyé lors de l'inscription -->
     <form class="bloc" action="ajout_membre.php" method="POST">
       <div class="row">
+        <!-- Choix du login -->
         <div class="form-group col-sm-6">
           <label for="name"><strong>Choisissez un login :</strong></label>
           <input type="text" class="form-control" name="login_entre" placeholder="Votre login" required>
         </div>
+        <!-- Choix du mot de passe -->
         <div class="form-group col-sm-6">
-          <label for="password"><strong>Choisissez un mot de passe (chiffres uniquement) :</strong></label>
+          <label for="password"><strong>Choisissez un mot de passe :</strong></label>
           <input type="password" class="form-control" name="pass_entre" placeholder="Votre mot de passe" required>
         </div></div>
+        <!-- Choix du statut -->
         <div>
           <label for="Statut"><strong>Choisissez votre statut :</strong></label>
           <br/>
@@ -30,6 +35,7 @@
           <label class="radio" for="joueur">Joueur</label>
         </div>
 
+      <!-- Bouton d'envoie du formulaire -->
       <div class="bloc_bouton">
       <button type="submit" name="connexion"  class="bouton1">Valider</button>
       </div>
